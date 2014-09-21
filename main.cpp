9dbe9baa87813435a07e7c2697936bdcd2bb7463
Sean-Ericson <sericson314@gmail.com>
2014-09-21 15:11:59 -0700

    First commit; currently no code.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Sean
+ *
+ * Created on September 21, 2014, 2:51 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
