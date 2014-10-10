cbb6a7c1d2383498dea7da06ae2807dd2d373933
Sean-Ericson <sericson314@gmail.com>
2014-10-09 22:06:30 -0700

    Seventh commit; Maybe I shouldn't doubt myself...or maybe I should. The game itself is done, but there is no AI. Six days until the rough draft is due...

--- Connect_Four_Game.cpp
+++ Connect_Four_Game.cpp
@@ -1,4 +1,13 @@
 #include "Connect_Four_Game.hpp"
+#include <iostream>
+#include <stdexcept>
+
+
+Move::Move()
+{}
+
+Move::Move(int p, int c, int r) : player(p), column(c), row(r)
+{}
 
 /*****************************/
 /* Board Class Methods       */
@@ -7,14 +16,9 @@
 Board::Board()
 {
     clearBoard();
-    lastMove.cloumn = -1;
+    lastMove.column = -1;
     lastMove.row = -1;
-    lastMove.player = NoPlayer;
-}
-
-Board::~Board()
-{
-    
+    lastMove.player = NO_PLR;
 }
 
 bool Board::isEmpty()
@@ -23,7 +27,7 @@ bool Board::isEmpty()
     {
         for (register int j = 0; j < ROWS; ++j)
         {
-            if (getPiece(i, j) != NoPlayer)
+            if (getPiece(i, j) != NO_PLR)
                 return false;
         }
     }
@@ -37,7 +41,7 @@ bool Board::isFull()
     {
         for (register int j = 0; j < ROWS; ++j)
         {
-            if (getPiece(i, j) == NoPlayer)
+            if (getPiece(i, j) == NO_PLR)
                 return false;
         }
     }
@@ -50,7 +54,7 @@ void Board::clearBoard()
     for (register int i = 0; i < COLS; ++i)
     {
         for (register int j = 0; j < ROWS; ++j)
-            placePiece(i, j, NoPlayer, true);
+            placePiece(i, j, NO_PLR, true);
     }
 }
 
@@ -58,13 +62,13 @@ bool Board::columnFull(int col)
 {
     if ( (col < 0) || (col >= COLS) )
     {
-        throw std::runtime_error("Invalid \"Board::columnFull\" function argument");
+        throw std::runtime_error("Error: invalid \"Board::columnFull\" function argument");
         return false;
     }
     
     for (register int i = 0; i < ROWS; ++i)
     {
-        if (getPiece(col, i) == NoPlayer)
+        if (getPiece(col, i) == NO_PLR)
             return false;
     }
     
@@ -75,35 +79,34 @@ int Board::getPiece(int col, int row)
 {
     if ( (col < 0) || (col >= COLS) || (row < 0) ||(row >= ROWS) )
     {
-        throw std::runtime_error("Error: invalid \"Board::getPiece\" function argument");
-        return NoPlayer;
+        return NO_PLR;
     }
     
-    if ( (state[col][row] != NoPlayer) && (state[col][row] != Player1) && 
-         (state[col][row] != Player2) )
+    if ( (state[col][row] != NO_PLR) && (state[col][row] != PLR_1) && 
+         (state[col][row] != PLR_2) )
     {
         throw std::runtime_error("Error: unexpected state value");
-        return NoPlayer;
+        return NO_PLR;
     }
     
     return state[col][row];
 }
 
-bool Board::placePiece(int col, int row, int player, bool replace = false)
+bool Board::placePiece(int col, int row, int player, bool replace)
 {
     if ( (col < 0) || (col >= COLS) || (row < 0) || (row >= ROWS) ||
-         (player < NoPlayer) || (player > Player2) )
+         (player < NO_PLR) || (player > PLR_2) )
     {
         throw std::runtime_error("Error: invalid \"Board::placePiece\" function argument");
         return false;
     }
     
-    if ( (replace == false) && (state[col][row] != NoPlayer) )
+    if ( (replace == false) && (state[col][row] != NO_PLR) )
         return false;
     
     state[col][row] = player;
     
-    lastMove.cloumn = col;
+    lastMove.column = col;
     lastMove.row = row;
     lastMove.player = player;
     
@@ -120,8 +123,8 @@ ConnectFourGame::ConnectFourGame()
     p1Score = 0;
     p2Score = 0;
     round = 0;
-    roundWinner = NoPlayer;
-    activePlayer = Player1;
+    roundWinner = NO_PLR;
+    activePlayer = PLR_1;
     p1Type = HUMAN;
     p2Type = HUMAN;
     roundWon = false;
@@ -133,18 +136,18 @@ ConnectFourGame::ConnectFourGame()
 
 bool ConnectFourGame::addPoints(int player, int points)
 {
-    if ( (player < Player1) || (player > Player2) )
+    if ( (player < PLR_1) || (player > PLR_2) )
     {
         std::runtime_error("Error: invalid \"ConnectFourGame::addPoints\" function argument");
         return false;
     }
 
-    if (player == Player1)
+    if (player == PLR_1)
     {
         p1Score += points;
     }
 
-    if (player == Player2)
+    if (player == PLR_2)
     {
         p2Score += points;
     }
@@ -164,9 +167,12 @@ bool ConnectFourGame::addPoints(int player, int points)
     }
 }*/
 
-int ConnectFourGame::checkWin(int player, int col, int row)
+int ConnectFourGame::checkWin(Move lastMove)
 {
     int score = 0;
+    int col = lastMove.column;
+    int row = lastMove.row;
+    int player = lastMove.player;
 
     if( (board.getPiece(col, row-1) == player) &&  // x
         (board.getPiece(col, row-2) == player) &&  // o
@@ -254,10 +260,10 @@ int ConnectFourGame::checkWin(int player, int col, int row)
 
     if ( board.isFull() )
     {
-        return Tie;
+        return TIE;
     }
 
-    return NoPlayer;
+    return NO_PLR;
 }
 
 int ConnectFourGame::dropPiece(int col, int player)
@@ -289,11 +295,11 @@ int ConnectFourGame::dropPiece(int col, int player)
 
 int ConnectFourGame::getScore(int player)
 {
-    if (player == Player1)
+    if (player == PLR_1)
     {
         return p1Score;
     }
-    else if (player == Player2)
+    else if (player == PLR_2)
     {
         return p2Score;
     }
@@ -301,13 +307,33 @@ int ConnectFourGame::getScore(int player)
     return -1;
 }
 
+int ConnectFourGame::getPlayerType(int player)
+{
+    if ((player != PLR_1) && (player != PLR_2))
+    {
+        throw std::runtime_error("Error: invalid \"ConnectFourGame::getPlayerType\" function argument");
+        return -1;
+    }
+    
+    if (player == PLR_1)
+        return p1Type;
+    
+    if (player == PLR_2)
+        return p2Type;
+}
+
+Move ConnectFourGame::getLastMove(void)
+{
+    return moves.back();
+}
+
 bool ConnectFourGame::setNewRound(void)
 {
     board.clearBoard();
     ++round;
-    roundWinner = NoPlayer;
+    roundWinner = NO_PLR;
     roundWon = false;
-    activePlayer = ( (round % 2) == 1) ? Player1: Player2;
+    activePlayer = ( (round % 2) == 1) ? PLR_1: PLR_2;
     newRound = true;
     
     moves.clear();
@@ -315,14 +341,14 @@ bool ConnectFourGame::setNewRound(void)
     return true;
 }
 
-void ConnectFourGame::setPlayerType(int player, bool type)
+void ConnectFourGame::setPlayerType(int player, int type)
 {
-    if (player == Player1)
+    if (player == PLR_1)
     {
         p1Type = type;
     }
     
-    if (player == Player2)
+    if (player == PLR_2)
     {
         p2Type = type;
     }
@@ -330,8 +356,8 @@ void ConnectFourGame::setPlayerType(int player, bool type)
 
 bool ConnectFourGame::setRoundWinner(int winner)
 {
-    if ( (winner != Tie) && (winner != NoPlayer) && 
-         (winner != Player1) && (winner != Player2) )
+    if ( (winner != TIE) && (winner != NO_PLR) && 
+         (winner != PLR_1) && (winner != PLR_2) )
     {
         return false;
     }
